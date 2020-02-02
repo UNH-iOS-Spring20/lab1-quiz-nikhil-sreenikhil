eca63501b01412112abb3896064fd6fcf495ef3f
sreenikhil <skoll8@unh.newhaven.edu>
2020-02-02 11:53:16 -0500

    Basic UI Elements created

--- Quiz3/ViewController.swift
+++ Quiz3/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
-
+import os
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //Interface Builder
+    @IBOutlet var questionLabel: UILabel!
+     @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        os_log("Showing next question")
     }
-
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        os_log("Showing the answer")
+    }
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        os_log("I Just loaded")
+        questionLabel.text = "I am a question"
+        answerLabel.text = "I am an answer"
+    }
+    
 
 }
 
